bf614122f218c87257df5a93d75a177c7e78eda4
Juan Palomino <juniorpalo16@gmail.com>
2018-05-08 19:13:47 -0600

    my server works

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const db = require('monk')('mongodb://admin:password@ds048878.mlab.com:48878/tvdemodb')
+const showsCollection = db.get('shows')
 
 const app = express()
 
@@ -16,49 +18,15 @@ app.use((req, res, next) => {
     next()
 })
 
-const inMemoryDatabase = {
-    shows: [
-        {
-            name: 'Rick and Morty',
-            rating: 5,
-            newPreviewImage: 'https://static.posters.cz/image/750/plakaty/rick-and-morty-watch-i50046.jpg'
-        },
-
-        {
-            name: 'Avatar the Last Airbender',
-            rating: 2,
-            newPreviewImage: 'https://ia.media-imdb.com/images/M/MV5BNzZlZmQyYTgtOWNmMy00NTNhLTgyOTYtNjhiOTllOGU2MDg5XkEyXkFqcGdeQXVyMjYxMzY2NDk@._V1_UY268_CR0,0,182,268_AL_.jpg'
-        },
-
-        {
-            name: 'One Punch Man',
-            rating: 4,
-            newPreviewImage: 'https://www.rightstufanime.com/images/productImages/699858578788_merchandise-saitame-genos-one-ounch-man-throw-blanket-primary.jpg?resizeid=3&resizeh=600&resizew=600'
-        },
-
-        {
-            name: 'Daredevil',
-            rating: 5,
-            newPreviewImage: 'https://ia.media-imdb.com/images/M/MV5BNzUwMDEyMTIxM15BMl5BanBnXkFtZTgwNDU3OTYyODE@._V1_UX182_CR0,0,182,268_AL_.jpg'
-        },
-
-        {
-            name: 'Ash vs Evil Dead',
-            rating: 5,
-            newPreviewImage: 'http://assets.starz.com/imgix/OriginalsPicker/eds2_598x336_v2.jpg'
-        }
-
-    ]
-}
-
-app.get('/shows', (req, res) => {
-    res.send(inMemoryDatabase.shows)
+app.get('/shows', async (req, res) => {
+    const shows = await showsCollection.find({})
+    res.send(shows)
 })
 
-app.post('/shows', (req, res) => {
+app.post('/shows', async (req, res) => {
     const newShow = req.body
-    inMemoryDatabase.shows.push(newShow)
-    res.send(newShow)
+    const savedShow = await showsCollection.insert(newShow)
+    res.send(savedShow)
 })
 
 app.listen('3001', () => console.log('running on 3001'))
